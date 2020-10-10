e9c457e479508c3bdfbc4ccf397c7a677709e231
Tobias Kronthaler <kronthto@gmail.com>
2020-10-10 14:07:28 +0200

    Service

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::ops::{DerefMut, Deref};
 use std::sync::{RwLock,  RwLockReadGuard, RwLockWriteGuard};
 use std::thread;
 use std::io::{stdin, stdout, Write};
+use std::env;
 
 use hash_hasher::{HashBuildHasher, HashedSet};
 
@@ -43,8 +44,8 @@ fn ask_input(q: &str) -> String {
     s
 }
 
-fn ask_playerstate() -> PlayerState {
-    match ask_input("Playerstate (S or A): ").as_str() {
+fn ask_playerstate(input: &String) -> PlayerState {
+    match input.as_str() {
         "S" => PlayerState::SWORD,
         "A" => PlayerState::ARMOR,
         _ => panic!("PlayerState must be S or A")
@@ -52,33 +53,39 @@ fn ask_playerstate() -> PlayerState {
 }
 
 fn main() {
-    // let game = D15Game::new(50, Point { x: 2, y: 5 },  Point { x: 4, y: 6 },  Point { x: 0, y: 7 },  Point { x: 4, y: 5 },  Point { x: 3, y: 5 }, PlayerState::ARMOR); // att 7
-   // let game = D15Game::new(55, Point { x: 3, y: 5 },  Point { x: 1, y: 7 },  Point { x: 7, y: 6 },  Point { x: 0, y: 4 },  Point { x: 2, y: 2 }, PlayerState::ARMOR); // att 5
-    // let game = D15Game::new(80, Point { x: 2, y: 5 },  Point { x: 4, y: 5 },  Point { x: 5, y: 3 },  Point { x: 7, y: 7 },  Point { x: 2, y: 2 }, PlayerState::ARMOR); // att 7 80
-    // let game = D15Game::new(109, Point { x: 2, y: 5 },  Point { x: 3, y: 5 },  Point { x: 5, y: 4 },  Point { x: 7, y: 6 },  Point { x: 5, y: 5 }, PlayerState::ARMOR); // att 7 80
-    // let game = D15Game::new(128, Point { x: 4, y: 5 },  Point { x: 6, y: 4 },  Point { x: 4, y: 6 },  Point { x: 3, y: 3 },  Point { x: 2, y: 4 }, PlayerState::SWORD); // att 2
-    // let game = D15Game::new(130, Point { x: 4, y: 5 },  Point { x: 7, y: 1 },  Point { x: 2, y: 4 },  Point { x: 3, y: 0 },  Point { x: 6, y: 6 }, PlayerState::ARMOR); // att 4
-    //let game = D15Game::new(130, Point { x: 2, y: 5 },  Point { x: 4, y: 5 },  Point { x: 2, y: 4 },  Point { x: 5, y: 6 },  Point { x: 2, y: 3 }, PlayerState::ARMOR); // OMG
-    // let game = D15Game::new(130, Point { x: 5, y: 5 }, Point { x: 4, y: 5 }, Point { x: 2, y: 4 }, Point { x: 5, y: 6 }, Point { x: 2, y: 3 }, PlayerState::ARMOR); // OMG 5/5
-
     let cpus = (num_cpus::get() / 2) + 1;
 
-    let players_state = ask_playerstate();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("1 arg expected")
+    }
+
+    let split = args[1].split("_");
+    let res: Vec<String> = split.map(|s| s.to_string()).collect();
+
+    if res.len() != 12 {
+        panic!("Invalid input")
+    }
+
+    let hp : i16 = res[0].parse().unwrap();
+    if hp > 128 || hp < 62 {
+        panic!("Invalid start hp")
+    }
+    let players_state = ask_playerstate(&res[11]);
 
      let game = D15Game::new(
-         ask_input("Enter HP: ").parse().unwrap(),
-         Point { x: ask_input("Enter Boss X: ").parse().unwrap(), y: ask_input("Enter Boss Y: ").parse().unwrap() },
-         Point { x: ask_input("Enter Player X: ").parse().unwrap(), y: ask_input("Enter Player Y: ").parse().unwrap() },
-         Point { x: ask_input("Enter Cat X: ").parse().unwrap(), y: ask_input("Enter Cat Y: ").parse().unwrap() },
-         Point { x: ask_input("Enter Dog X: ").parse().unwrap(), y: ask_input("Enter Dog Y: ").parse().unwrap() },
-         Point { x: ask_input("Enter Dragon X: ").parse().unwrap(), y: ask_input("Enter Dragon Y: ").parse().unwrap() },
+         hp,
+         Point { x: res[1].parse().unwrap(), y: res[2].parse().unwrap() },
+         Point { x: res[3].parse().unwrap(), y: res[4].parse().unwrap() },
+         Point { x: res[5].parse().unwrap(), y: res[6].parse().unwrap() },
+         Point { x: res[7].parse().unwrap(), y: res[8].parse().unwrap() },
+         Point { x: res[9].parse().unwrap(), y: res[10].parse().unwrap() },
          players_state
      );
 
     let mut possible_start_moves = game.get_possible_moves();
     let num_threads = min(possible_start_moves.len(), cpus) - 2;
-
-    println!("-> Press Ctrl+C to exit");
 
     let index_switch = possible_start_moves.iter().position(|x| *x == Move::SWITCH).unwrap();
     possible_start_moves.remove(index_switch);
@@ -101,7 +108,6 @@ fn main() {
     let mut remainder_moves = possible_start_moves.to_vec();
     remainder_moves.drain(0..num_threads);
     threads.push(   thread::spawn(move || {
-        println!("Spawned remainder Thread");
         for move_oper in remainder_moves {
             let mut new_game = game.clone();
             new_game.do_move(&move_oper);
@@ -113,10 +119,6 @@ fn main() {
     for t in threads {
         t.join().expect("Thread panicked");
     }
-
-    println!("Done / all checked");
-
-    let _ = stdin().read_line(&mut String::new()).unwrap(); // keep console open
 }
 
 fn solve(game: &D15Game, moves_done: &mut Vec<Move>) {
@@ -127,7 +129,6 @@ fn solve(game: &D15Game, moves_done: &mut Vec<Move>) {
         return;
     }
     if game.check_win() {
-        println!("WIN!!!");
         print_result_moves(moves_done);
         unsafe { FINISHED = true; }
         return;
@@ -146,10 +147,12 @@ fn solve(game: &D15Game, moves_done: &mut Vec<Move>) {
         let mut checked_perms_guard_write: RwLockWriteGuard<HashedSet<u64>> = CHECKED_PERMUTATIONS.write().unwrap();
         let checked_perms_write = checked_perms_guard_write.deref_mut();
         checked_perms_write.insert(game_hash);
+        /*
         let seen_boards = checked_perms_write.len();
         if seen_boards % 1000000 == 0 {
             println!("Seen {} boards", seen_boards);
         }
+        */
     }
 
     let moves = game.get_possible_moves();
@@ -166,15 +169,15 @@ fn solve(game: &D15Game, moves_done: &mut Vec<Move>) {
 fn print_result_moves(moves: &Vec<Move>) {
     for move_oper in moves {
         match move_oper {
-            Move::PASSTURN => println!("PASSTURN"),
-            Move::LEFT => println!("LEFT"),
-            Move::RIGHT => println!("RIGHT"),
-            Move::UP => println!("UP"),
-            Move::DOWN => println!("DOWN"),
-            Move::DOG => println!("DOG"),
-            Move::CAT => println!("CAT"),
-            Move::DRAGON => println!("DRAGON"),
-            Move::SWITCH => println!("SWITCH"),
+            Move::PASSTURN => print!("PASSTURN_"),
+            Move::LEFT => print!("LEFT_"),
+            Move::RIGHT => print!("RIGHT_"),
+            Move::UP => print!("UP_"),
+            Move::DOWN => print!("DOWN_"),
+            Move::DOG => print!("DOG_"),
+            Move::CAT => print!("CAT_"),
+            Move::DRAGON => print!("DRAGON_"),
+            Move::SWITCH => print!("SWITCH_"),
         };
     }
 }
